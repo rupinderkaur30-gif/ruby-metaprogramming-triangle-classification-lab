68cdb42872c98070a821fabf3c1362727e85d825
rupinderkaur30-gif <rupi063094@gmail.com>
2021-03-04 10:22:38 -0500

    commit tree 6fe7ac7ce94431370ad79ae2692a1fb7962076c4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+attr_accessor :a , :b, :c
+
+def initialize(a, b, c)
+  @a = a 
+  @b = b 
+  @c = c
 end
+
+def kind
+  if (@a <= 0 && @b <= 0 && @c <= 0 )|| (@a + @b <= @c) || (@b + @c <= a) || (@c + @a <= @b)
+    begin
+    raise TriangleError
+    puts error.message
+    end
+elsif (@a == @b && @b == @c)
+  :equilateral
+elsif @a == @b||@b == @c || @a == @c 
+  :isosceles
+else
+   :scalene
+
+end
+end
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
+  end
+end
+end
+
+
+
+
+
+
